5f2ddee57c7d02e44bdcef3363efbb63893b3293
Bethel-Iyke <talk2bethel017@gmail.com>
2022-06-02 12:22:49 -0700

    script that s compiler

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+1 #include <stdio.h>
+2 /**
+3 * main - Prints all data types
+4 *
+5 * Description: prints all data types in c programming
+6 *
+7 * Return: 0
+8 *
+9 */
+10 int main(void)
+11 {
+12 char a;
+13 int b;
+14 int longc;
+15 int long longd;
+16 prints ("Size of a char: %zu  byte(s)\n", sizeof(a));
+17 printf(Size of an int: %zu byte(s)\n", sizeof(b));
+18 printf(Size of a long int: %zu byte(s)\n", sizeof(c));
+19 printf(Size of a long long int: %zu byte(s)\n", sizeof(d);
+20 printf(Size of a float: %zu byte(s\n", sizeof(float);
+21 return(0);
+22}
