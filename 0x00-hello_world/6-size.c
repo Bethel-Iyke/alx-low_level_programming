5d67d67a196b1e3d07178205208f7173656c27e8
Bethel-Iyke <talk2bethel017@gmail.com>
2022-06-02 14:59:03 -0700

    scrit that prints various sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,22 +1,21 @@
-1 #include <stdio.h>
-2 /**
-3 * main - Prints all data types
-4 *
-5 * Description: prints all data types in c programming
-6 *
-7 * Return: 0
-8 *
-9 */
-10 int main(void)
-11 {
-12 char a;
-13 int b;
-14 int longc;
-15 int long longd;
-16 prints ("Size of a char: %zu  byte(s)\n", sizeof(a));
-17 printf(Size of an int: %zu byte(s)\n", sizeof(b));
-18 printf(Size of a long int: %zu byte(s)\n", sizeof(c));
-19 printf(Size of a long long int: %zu byte(s)\n", sizeof(d);
-20 printf(Size of a float: %zu byte(s\n", sizeof(float);
-21 return(0);
-22}
+#include <stdio.h>
+
+/**
+ * main - enter text point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+	long int b;
+	long long int c;
+	char d;
+	float f;
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+	return (0);
+}
