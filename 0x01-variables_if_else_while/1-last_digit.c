af3c11efcbc30a2d6b204878ef560974f996a6db
Bethel-Iyke <talk2bethel017@gmail.com>
2022-06-03 17:09:52 -0700

    script that assign

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,14 +10,13 @@
 
 int main(void)
 {
-	int n;
-	int last_digit;
+int n;
+int last_digit;
 
-	srand(time(0));
-
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	last_digit = n % 10;
+ srand(time(0));
+n = rand() - RAND_MAX / 2;
+/* your code goes there */
+ last_digit = n % 10;
 if (last_digit > 5)
 printf("Last digit of %d is %d and greater than 5\n", n, last_digit);
 else if  (last_digit == 0)
