a489a9d77f2f6d7545d3c332e3cb27a0f25b2c42
Bethel-Iyke <talk2bethel017@gmail.com>
2022-06-04 05:33:16 -0700

    script that print lowercae correct

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
-* main - prints the alphabet in lowercase,and then in uppercase.
+* main - Prints the alphabet in lowercase,and then in uppercase.
 *
 * Return: Always 0.
 */
@@ -9,7 +9,10 @@ int main(void)
 {
 char alpha;
 
-for (alpha = 'a'; alpha <= 'z' ; alpha++)
+for (alpha = 'a'; alpha <= 'z'; alpha++)
+putchar(alpha);
+
+for(alpha = 'A'; alpha <= 'Z'; alpha++)
 putchar(alpha);
 putchar('\n');
 return (0);
