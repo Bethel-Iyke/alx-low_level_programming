39680a9fbd3218583dbe10db617aa44bf7d07090
Bethel-Iyke <talk2bethel017@gmail.com>
2022-06-05 02:41:23 -0700

    program that prints gigit combination

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -4,6 +4,7 @@
  * main -Prints all possible combinations of two two-digit numbers,
  * ranging from 0-99, seperated by a comma followed by a space.
  *
+ * Return: Always return 0.
  */
 int main(void)
 {
