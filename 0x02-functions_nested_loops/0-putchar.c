96b825d88d48852ef2c34cdb19ccd0aa55c55def
Bethel-Iyke <talk2bethel017@gmail.com>
2022-06-07 14:46:14 -0700

    program that prints putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - Print the word "main"
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int i;
+char p[] = "main";
+
+for (i = 0; i < 9; i++)
+{
+_putchar(p[i]);
+}
+
+_putchar('\n');
+return (0);
+}
