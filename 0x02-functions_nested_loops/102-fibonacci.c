5714df2e621da0d1c32565ded35a269f422fcd1a
Bethel-Iyke <talk2bethel017@gmail.com>
2022-06-08 13:29:57 -0700

    prints fibonacci numbers

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,7 +8,7 @@ int main(void)
 {
 int count;
 unsigned long fib1 = 0, fib2 = 1, sum;
-for (count = 0; count <= 50; count++)
+for (count = 0; count < 50; count++)
 {
 sum = fib1 + fib2;
 printf("%lu", sum);
@@ -17,7 +17,7 @@ fib2 = sum;
 if (count == 49)
 printf("\n");
 else
-printf(",");
+printf(", ");
 }
 return (0);
 }
