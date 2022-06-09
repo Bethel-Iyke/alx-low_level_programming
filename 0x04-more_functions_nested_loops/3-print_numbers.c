e9f240660aad8c8f09f49d46f43701ac117ec7ce
Bethel-Iyke <talk2bethel017@gmail.com>
2022-06-09 15:53:06 -0700

    prints number 1 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_numbers - prints numbers from 1 to 9
+ * Return: returs 1 or 0
+ */
+
+void print_numbers(void)
+{
+int a;
+for (a = '0'; a <= '9'; a++)
+_putchar(a);
+_putchar('\n');
+}
