91923e032e046ee19f727560aa8d97b18410cc6e
Bethel-Iyke <talk2bethel017@gmail.com>
2022-06-10 09:35:56 -0700

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,9 +1,8 @@
 #include "main.h"
-
+void positive_or_negative(int i);
 /**
-* main -  tests function that prints if integer is positive or negative.
-* Return: 0.
-*
+* main - tests function that prints if integer is positive or negative
+* Return: 0
 */
 
 int main(void)
@@ -13,9 +12,7 @@ int main(void)
 int i;
 
 i = 0;
-
 positive_or_negative(i);
 
 return (0);
-
 }
