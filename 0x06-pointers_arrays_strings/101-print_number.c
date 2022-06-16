a2c587c0db28d5b42626058ab82950198836fb71
Bethel-Iyke <talk2bethel017@gmail.com>
2022-06-16 11:46:16 -0700

    add line of code

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -12,11 +12,15 @@ void print_number(int n)
 int count, y, k;
 y = 1000000000;
 if (n == 0)
+
 _putchar('0');
+
 else if (n > 0)
 n *= -1;
 else
+
 _putchar('-');
+
 for (count = 0; count < 10; count++, y /= 10)
 {
 if (n / y == 0)
