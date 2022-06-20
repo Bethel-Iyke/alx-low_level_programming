4c1204291c4ffa372a40c17080bfa6240a1f3bfd
Bethel-Iyke <talk2bethel017@gmail.com>
2022-06-20 10:37:47 -0700

    poits to memory areas

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * *_memset - fills a block of memory with constant byte
+ * @s: starting address of memory to be filled
+ * @b: the desired value
+ * @n: number of bytes to be changed
+ *
+ * Return: a pointer to memory areas
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+return (memset(s + n, b, n * sizeof(char)));
+}
