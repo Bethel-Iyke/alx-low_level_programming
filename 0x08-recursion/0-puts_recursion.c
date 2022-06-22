a627f24f58a01056503fda1fd9fa272c72c2a5ce
Bethel-Iyke <talk2bethel017@gmail.com>
2022-06-22 07:29:08 -0700

    prints a string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _puts - prints strings with puts
+ * @str: char string input
+ * Return: Always 0
+ */
+
+void _puts(char *str)
+
+{
+_puts("Puts with recursion\n");
+
+}
